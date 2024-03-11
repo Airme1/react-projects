f8ffa63b0dd2f43062e4f5c5a8b72b7ef000a09f
Airme1 <emmanuelochoja@yahoo.com>
2024-03-11 09:54:12 +0100

    Birthday

--- birthday/src/App.jsx
+++ birthday/src/App.jsx
@@ -1,6 +1,12 @@
+import React from "react"
+import List from "./List"
+import data from "./components/data"
 
-export default function App(){
-  return(
-    <h1>Hello</h1>
+export default function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <List people={data} />
+    </>
   )
 }
--- birthday/src/List.jsx
+++ birthday/src/List.jsx
@@ -1,12 +1,15 @@
-export default function List({people}){
-    const {id, name, age, image} = people;
-    return(
+export default function List({ people }) {
+    people.map((person) => {
+        const {id, name, age, image } = person;
+    })
+    
+    return (
         <>
-        <img src={image} alt={name} />
-        <div className="contact">
-            <h1>{name}</h1>
-            <p>{age}</p>
-        </div>
+            <img src={image} alt={name} />
+            <div className="contact">
+                <h1>{name}</h1>
+                <p>{age}</p>
+            </div>
         </>
     )
 }
